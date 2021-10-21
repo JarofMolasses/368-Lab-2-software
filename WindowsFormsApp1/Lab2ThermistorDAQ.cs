e077182be0263a5a6fe64eab51f4a6c52768ab0d
mol20@student.ubc.ca <mol20@student.ubc.ca>
2021-10-20 23:57:01 -0700

    add documentation, fix rolling avg not actually changing window size

--- WindowsFormsApp1/Lab2ThermistorDAQ.cs
+++ WindowsFormsApp1/Lab2ThermistorDAQ.cs
@@ -20,14 +20,14 @@ namespace Lab2
         ConcurrentQueue<Int32> bufferQueue = new ConcurrentQueue<Int32>();
         Int32 dequeueOut; 
         ConcurrentQueue<float> voltsQueue = new ConcurrentQueue<float>();        // ADC converted voltages 
-        ConcurrentQueue<float> tempQueue = new ConcurrentQueue<float>();
-        ConcurrentQueue<float> tempAvgQueue = new ConcurrentQueue<float>();
+        ConcurrentQueue<float> tempQueue = new ConcurrentQueue<float>();         // Computed temperatures
+        ConcurrentQueue<float> tempAvgQueue = new ConcurrentQueue<float>();      // Copy of temperatures queue for a rolling average 
 
         
         static int tempWindowSize = 500;                                        // This is the memory depth for the graphical display: display this many temperature points
-        int rollingAvgSize = 300;                                        // number of samples for rolling average calculation
+        int rollingAvgSize = 300;                                               // number of samples for rolling average calculation
         float trash;                                                            // dequeue into this trash to maintain windowsize items in quueue
-        float[] tempDisplayCopyArray = new float[tempWindowSize];
+        float[] tempDisplayCopyArray = new float[tempWindowSize];               // copy tempQueue into this array for live grapihng at each frame
 
         int byteState = -1;                // 0 = waiting, 1 = MSB, 2 = LSB
         
@@ -92,7 +92,7 @@ namespace Lab2
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            serialPort1.PortName = "COM3";          // lazy, this is the default MSP430 port 
+            serialPort1.PortName = "COM3";          // this is the default MSP430 port 
           
             comPortDropdown.Items.Clear();
             comPortDropdown.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
@@ -130,7 +130,7 @@ namespace Lab2
             {
                 foreach(Int32 bufferItem in bufferQueue)
                 {
-                    bufferQueue.TryDequeue(out dequeueOut);
+                    bufferQueue.TryDequeue(out dequeueOut); 
 
                     if(dequeueOut == 255)
                     {
@@ -145,28 +145,28 @@ namespace Lab2
                             break;
 
                         case 1:
-                            ms5b = bufferItem;         // MSB
+                            ms5b = bufferItem;                   // store MS5B
                             Console.Write($"MS5B={Convert.ToString(ms5b, 10)}, ");
                             byteState = 2;
                             break;
 
                         case 2:
-                            ls5b = bufferItem;                   // LSB: add to sum
+                            ls5b = bufferItem;                   // store LS5B
                             Console.Write($"LS5B={Convert.ToString(bufferItem, 10)}, ");
 
-                            int adcReading = (ms5b << 5) | ls5b;          // add MSB << 5 and LSB 
-                            float voltage = Vcc * (adcReading / (float)analogRes);
-                            voltsQueue.Enqueue(voltage);
-                            thermistorVDisplay.Text = voltage.ToString();
+                            int adcReading = (ms5b << 5) | ls5b;                        // add MSB << 5 and LSB to reconstruct ADC reading
+                            float voltage = Vcc * (adcReading / (float)analogRes);      // compute voltage
+                            voltsQueue.Enqueue(voltage);                                // store voltage
+                            thermistorVDisplay.Text = voltage.ToString();               
 
-                            float temp = convertVoltsToTemperature(voltage);
+                            float temp = convertVoltsToTemperature(voltage);            // convert voltage to temperature
+                            tempQueue.Enqueue(temp);                                    // store temperature in both raw and avg queues
+                            tempAvgQueue.Enqueue(temp);
                             temperatureKDisplay.Text = temp.ToString();
                             temperatureCDisplay.Text = (temp - 273.15).ToString();
-                            tempQueue.Enqueue(temp);
 
-                            float tempAvg = tempQueue.Average();
+                            float tempAvg = tempAvgQueue.Average();                        // take the average 
                             tempKRollingAvgDisplay.Text = tempAvg.ToString();
-                            tempAvgQueue.Enqueue(tempAvg);
                             
                             if(tempQueue.Count > tempWindowSize)
                             {
@@ -174,7 +174,7 @@ namespace Lab2
                             }
                             if(tempAvgQueue.Count > rollingAvgSize)
                             {
-                                tempAvgQueue.TryDequeue(out trash);
+                                tempAvgQueue.TryDequeue(out trash);     // keeps the avg queue at rollingAvgSize items
                             }
 
                             if(saveFileCheckBox.Checked)            
@@ -195,17 +195,6 @@ namespace Lab2
             }
         }
 
-        private void updateRollingAvgSize()
-        {
-            rollingAvgSize = Convert.ToInt32(rollingAvgSizeTextBox.Text);            // allow for on the fly average window editing
-            if (rollingAvgSize > tempWindowSize)
-            {
-                rollingAvgSizeTextBox.Clear();
-                rollingAvgSize = tempWindowSize;
-                MessageBox.Show("Rolling avg size capped at temp window size");
-                rollingAvgSizeTextBox.Text = tempWindowSize.ToString();
-            }
-        }
         private void saveFileCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             if (saveFileCheckBox.Checked)
